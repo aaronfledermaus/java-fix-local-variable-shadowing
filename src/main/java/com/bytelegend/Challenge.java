361f44220057c729a398c73bd77febb9c71ad014
shiguoqing99 <3118482822@qq.com>
2021-12-07 12:35:30 +0000

    Update at 2021-12-07T12:35:30.732429Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,8 +13,8 @@ public class Challenge {
     }
 
     public static void addAverageOf(int a, int b) {
-        int sum = a + b;
+        sum = a + b;
         int average = sum / 2;
-        sum = sum + average;
+        sum = average;
     }
 }
