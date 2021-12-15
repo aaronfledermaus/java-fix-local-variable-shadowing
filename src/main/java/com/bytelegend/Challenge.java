b288a9786479aaaffdaa02c412afa5e4c2d44994
bwb408114278 <408114278@qq.com>
2021-12-15 09:22:20 +0000

    Update at 2021-12-15T09:22:20.069049Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,20 +1,20 @@
 package com.bytelegend;
 
 public class Challenge {
-    public static int sum = 0;
+    public static int total = 0;
 
     public static void main(String[] args) {
         addAverageOf(1, 3);
-        System.out.println("addAverageOf(1,3): " + sum);
+        System.out.println("addAverageOf(1,3): " + total);
         addAverageOf(2, 6);
-        System.out.println("addAverageOf(2,6): " + sum);
+        System.out.println("addAverageOf(2,6): " + total);
         addAverageOf(3, 5);
-        System.out.println("addAverageOf(3,5): " + sum);
+        System.out.println("addAverageOf(3,5): " + total);
     }
 
     public static void addAverageOf(int a, int b) {
         int sum = a + b;
         int average = sum / 2;
-        sum = sum + average;
+        total = total + average;
     }
 }
