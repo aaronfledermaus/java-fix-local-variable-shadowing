fed3e073490142c122b9b1063053ccba909784b0
o0hana <2487311872@qq.com>
2022-01-26 02:01:07 +0000

    Update at 2022-01-26T02:01:07.445804Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -5,16 +5,16 @@ public class Challenge {
 
     public static void main(String[] args) {
           System.out.println("addAverageOf(1,3): " + addAverageOf(1,3));
-		sum=addAverageOf(1,3);
+		//sum=addAverageOf(1,3);
         System.out.println("addAverageOf(2,6): " + addAverageOf(2,6));
-				sum=addAverageOf(2,6);
+				//sum=addAverageOf(2,6);
         System.out.println("addAverageOf(3,5): " + addAverageOf(3,5));
     }
 
     public static int addAverageOf(int a, int b) {
         int average = (a+b) / 2;
         //int sum =0;
-		//sum =+ average;
-		return sum + average;
+		sum =sum+ average;
+		return sum ;
     }
 }
