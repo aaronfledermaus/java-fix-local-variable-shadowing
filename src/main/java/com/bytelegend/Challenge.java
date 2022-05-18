21eca5b1f606ce28eac97069cd9651fcdbe6dab8
jamesmkinder <jmkgfk@gmail.com>
2022-05-18 03:56:08 +0000

    Update at 2022-05-18T03:56:08.436855Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,8 +13,8 @@ public class Challenge {
     }
 
     public static void addAverageOf(int a, int b) {
-        int sum = a + b;
-        int average = sum / 2;
+        int number = a + b;
+        int average = number / 2;
         sum = sum + average;
     }
 }
