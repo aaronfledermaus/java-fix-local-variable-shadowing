956894b785ede456d77f5fdcaaf85e3637233c5c
chinahzh <2987140244@qq.com>
2022-05-27 05:34:58 +0000

    Update at 2022-05-27T05:34:58.741742Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,20 +1,21 @@
 package com.bytelegend;
 
 public class Challenge {
-    public static int sum = 0;
+   
 
     public static void main(String[] args) {
         addAverageOf(1, 3);
-        System.out.println("addAverageOf(1,3): " + a);
+        System.out.println("addAverageOf(1,3): " + sum);
         addAverageOf(2, 6);
-        System.out.println("addAverageOf(2,6): " + a);
+        System.out.println("addAverageOf(2,6): " + sum);
         addAverageOf(3, 5);
-        System.out.println("addAverageOf(3,5): " + a);
+        System.out.println("addAverageOf(3,5): " + sum);
     }
 
     public static void addAverageOf(int a, int b) {
-        int sum = a + b;
-        int average = sum / 2;
-        a =  average;
+         public static int sum = 0;
+         sum = a + b;
+         int average = sum / 2;
+         sum =  average;
     }
 }
