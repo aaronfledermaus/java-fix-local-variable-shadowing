8779b5d797749e1182af84ce1252bae52e6a3eb8
batman405 <2638603343@qq.com>
2022-06-22 06:54:02 +0000

    Update at 2022-06-22T06:54:02.798727Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -4,11 +4,11 @@ public class Challenge {
     public static int sum = 0;
 
     public static void main(String[] args) {
-        addAverageOf(1, 3);
+        sum = addAverageOf(1, 3);
         System.out.println("addAverageOf(1,3): " + sum);
-        addAverageOf(2, 6);
+        sum = addAverageOf(2, 6);
         System.out.println("addAverageOf(2,6): " + sum);
-        addAverageOf(3, 5);
+        sum = addAverageOf(3, 5);
         System.out.println("addAverageOf(3,5): " + sum);
     }
 
