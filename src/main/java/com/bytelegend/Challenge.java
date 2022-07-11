9ae3387a7a83eee596703916b0ac74a1fc2332ce
Birlinha <omenaribeiro13@gmail.com>
2022-07-11 03:50:52 +0000

    Update at 2022-07-11T03:50:52.763327Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,7 +2,6 @@ package com.bytelegend;
 
 public class Challenge {
     public static void main(String[] args) {
-        int sum;
         addAverageOf(1, 3);
         System.out.println("addAverageOf(1,3): " + sum);
         addAverageOf(2, 6);
