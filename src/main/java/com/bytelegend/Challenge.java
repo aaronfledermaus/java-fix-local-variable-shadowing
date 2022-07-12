2a80d7620e1a2965e2fcb2dc87618aa1400ce9ba
chesterbachelor <kawalerbartosz@gmail.com>
2022-07-12 15:16:40 +0000

    Update at 2022-07-12T15:16:40.005788Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,20 +1,20 @@
 package com.bytelegend;
 
 public class Challenge {
-    public static int sum = 0;
+    public static int sumAvg = 0;
 
     public static void main(String[] args) {
         addAverageOf(1, 3);
-        System.out.println("addAverageOf(1,3): " + sum);
+        System.out.println("addAverageOf(1,3): " + sumAvg);
         addAverageOf(2, 6);
-        System.out.println("addAverageOf(2,6): " + sum);
+        System.out.println("addAverageOf(2,6): " + sumAvg);
         addAverageOf(3, 5);
-        System.out.println("addAverageOf(3,5): " + sum);
+        System.out.println("addAverageOf(3,5): " + sumAvg);
     }
 
     public static void addAverageOf(int a, int b) {
         int sum = a + b;
         int average = sum / 2;
-        sum = sum + average;
+        sumAvg = sum + average;
     }
 }
