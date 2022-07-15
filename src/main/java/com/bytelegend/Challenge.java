cb5db4971939d9fc23449f11d2082a7e54ee548a
GOH73 <goh73@outlook.com>
2022-07-15 06:45:38 +0000

    Update at 2022-07-15T06:45:38.332294Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -4,16 +4,16 @@ public class Challenge {
     public static int sum = 0;
 
     public static void main(String[] args) {
-        sum = addAverageOf(1, 3);
+        addAverageOf(1, 3);
         System.out.println("addAverageOf(1,3): " + sum);
-        sum = addAverageOf(2, 6);
+        addAverageOf(2, 6);
         System.out.println("addAverageOf(2,6): " + sum);
-        sum = addAverageOf(3, 5);
+        addAverageOf(3, 5);
         System.out.println("addAverageOf(3,5): " + sum);
     }
 
     public static void addAverageOf(int a, int b) {
-        int sum = a + b;
+        sum = a + b;
         int average = sum / 2;
         sum = sum + average;
     }
