cbdb6f37d8445992e45b27d9e0a69809c33e7625
tassellll <2112562745@qq.com>
2022-07-16 01:25:10 +0000

    Update at 2022-07-16T01:25:10.867758Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,8 +13,6 @@ public class Challenge {
     }
 
     public static void addAverageOf(int a, int b) {
-        int sum = a + b;
-        int average = sum / 2;
-        sum = sum + average;
+        sum += (a + b)/2;
     }
 }
