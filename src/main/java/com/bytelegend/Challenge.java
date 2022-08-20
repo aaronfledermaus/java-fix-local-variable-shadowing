b6ef69a90a586402893757b5efa82dbb896ac617
MisterToma <ktoma@sandi.net>
2022-08-20 15:49:39 +0000

    Update at 2022-08-20T15:49:39.837993Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -15,6 +15,6 @@ public class Challenge {
     public static void addAverageOf(int a, int b) {
         int total = a + b;
         int average = total / 2;
-        sum = total + average;
+        Challenge.sum = total + average;
     }
 }
