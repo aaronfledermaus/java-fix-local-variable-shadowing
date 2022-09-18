d5b7a823c15537031f4e3b0e328ae0db0e1b90f2
YuanHao97 <y1997h@126.com>
2022-09-18 11:14:13 +0000

    Update at 2022-09-18T11:14:13.520823Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,8 +13,8 @@ public class Challenge {
     }
 
     public static void addAverageOf(int a, int b) {
-        int sum = a + b;
-        int average = sum / 2;
+        int s = a + b;
+        int average = s / 2;
         sum = sum + average;
     }
 }
