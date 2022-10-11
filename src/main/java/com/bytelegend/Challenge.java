6eaeec62ea391b89f6a07fbd7683a5f0ec83e165
tiger0961 <29515547@qq.com>
2022-10-11 07:30:42 +0000

    Update at 2022-10-11T07:30:42.074506Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,8 +13,8 @@ public class Challenge {
     }
 
     public static void addAverageOf(int a, int b) {
-        sum = a + b;
-        int average = sum / 2;
-        sum = sum - average;
+        int c = a + b;
+        int average = c / 2;
+        sum = sum + average;
     }
 }
