d6735f1e0b6b4ad21af4418e02849b7c00cad706
bdjdndn <909439250@qq.com>
2022-11-05 14:48:32 +0000

    Update at 2022-11-05T14:48:32.400897Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,7 +13,7 @@ public class Challenge {
     }
 
     public static void addAverageOf(int a, int b) {
-        ii = a + b;
+        i = a + b;
         int average = i / 2;
         sum = sum + average;
         
