b9e3d1adef0be0756586b2dfb6a27e244c8b9892
guda04021 <1349308535@qq.com>
2022-11-09 09:47:58 +0000

    Update at 2022-11-09T09:47:58.603036Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,7 +2,6 @@ package com.bytelegend;
 
 public class Challenge {
     public static int sum = 0;
-    public static int s;
 
     public static void main(String[] args) {
         addAverageOf(1, 3);
@@ -14,9 +13,8 @@ public class Challenge {
     }
 
     public static void addAverageOf(int a, int b) {
-        sum = a + b;
+        int sum = a + b;
         int average = sum / 2;
-        sum = s + average;
-        s = sum;
+        this.sum = sum + average;
     }
 }
