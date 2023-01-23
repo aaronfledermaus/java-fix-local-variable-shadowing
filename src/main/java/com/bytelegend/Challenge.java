69b6779c44bb10ba2361383ec23dea64b620e2bf
CITVAIM <citvaim@icloud.com>
2023-01-23 14:17:18 +0000

    Update at 2023-01-23T14:17:18.907391Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,15 +6,17 @@ public class Challenge {
     public static void main(String[] args) {
         sum = addAverageOf(1, 3);
         System.out.println("addAverageOf(1,3): " + sum);
-        sum = addAverageOf(2, 6) + sum;
+        sum = addAverageOf(2, 6);
         System.out.println("addAverageOf(2,6): " + sum);
-        sum = addAverageOf(3, 5) + sum;
+        sum = addAverageOf(3, 5);
         System.out.println("addAverageOf(3,5): " + sum);
     }
 
     public static int addAverageOf(int a, int b) {
-        int sum = a + b;
-        int average = sum / 2;
+//        sum = a + b;
+        int average = (a + b) / 2;
+        average = average + sum;
+//        System.out.println(average);
         return average;
     }
 }
