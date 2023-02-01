88de2e6660f8dab95435585f3f752a9c5dc43a8f
AaronFledermaus <78683536+aaronfledermaus@users.noreply.github.com>
2023-02-01 14:15:00 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -13,7 +13,7 @@ public class Challenge {
     }
 
     public static void addAverageOf(int a, int b) {
-        int sum = a + b;
+        private int sum = a + b;
         int average = sum / 2;
         sum = sum + average;
     }
